52f97dc3f9fd25b86c2b93903c103c6766b088d5
Henning Schwentner <hs@wps.de>
2019-08-19 18:24:48 +0200

    Implement InboxApplicationServiceImpl.java

--- leasingninja-riskmanagement/src/main/java/io/leasingninja/riskmanagement/application/InboxApplicationServiceImpl.java
+++ leasingninja-riskmanagement/src/main/java/io/leasingninja/riskmanagement/application/InboxApplicationServiceImpl.java
@@ -1,11 +1,28 @@
 package io.leasingninja.riskmanagement.application;
 
+import io.leasingninja.riskmanagement.domain.Contract;
+import io.leasingninja.riskmanagement.domain.ContractNumber;
+import io.leasingninja.riskmanagement.domain.ContractRepository;
+import io.leasingninja.riskmanagement.domain.SignDate;
 import org.springframework.stereotype.Component;
+
+import static java.time.temporal.ChronoField.*;
 
 @Component
 class InboxApplicationServiceImpl implements InboxApplicationService {
+
+	private ContractRepository contracts;
+
+	public InboxApplicationServiceImpl(ContractRepository contracts) {
+		this.contracts = contracts;
+	}
+
 	@Override
 	public void confirmSignedContract(String lvnr, int year, int month, int dayOfMonth) {
-		
+		YEAR.checkValidValue(year);
+		MONTH_OF_YEAR.checkValidValue(month);
+		DAY_OF_MONTH.checkValidValue(dayOfMonth);
+
+		contracts.save(new Contract(ContractNumber.of(lvnr), SignDate.of(year, month, dayOfMonth)));
 	}
 }
--- leasingninja-riskmanagement/src/test/java/io/leasingninja/riskmanagement/application/InboxApplicationServiceTest.java
+++ leasingninja-riskmanagement/src/test/java/io/leasingninja/riskmanagement/application/InboxApplicationServiceTest.java
@@ -1,14 +1,39 @@
 package io.leasingninja.riskmanagement.application;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.refEq;
+import static org.mockito.BDDMockito.then;
 
+import io.leasingninja.riskmanagement.domain.Contract;
+import io.leasingninja.riskmanagement.domain.ContractNumber;
+import io.leasingninja.riskmanagement.domain.ContractRepository;
+import io.leasingninja.riskmanagement.domain.SignDate;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class InboxApplicationServiceTest {
 
+	@Mock
+	private ContractRepository contracts;
+
+	@InjectMocks
+	private InboxApplicationServiceImpl inboxUnderTest;
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void givenAnEmptyInbox_whenConfirmSignedContract_thenContractIsSaved() {
+		// Given
+
+		// When
+		inboxUnderTest.confirmSignedContract("4711", 2019, 12, 24);
+
+		// Then
+		then(contracts).should().save(refEq(new Contract(
+				ContractNumber.of("4711"),
+				SignDate.of(2019, 12, 24))));
 	}
 
 }
