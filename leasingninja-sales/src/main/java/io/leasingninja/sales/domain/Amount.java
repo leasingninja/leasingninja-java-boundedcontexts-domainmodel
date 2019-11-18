eb732bd600449c0d965e65f5df9c7ee892a431cc
Henning Schwentner <hs@wps.de>
2019-11-18 15:40:41 +0100

    Moved hashCode() after equals()

--- leasingninja-sales/src/main/java/io/leasingninja/sales/domain/Amount.java
+++ leasingninja-sales/src/main/java/io/leasingninja/sales/domain/Amount.java
@@ -33,15 +33,6 @@ public class Amount {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + amount;
-		result = prime * result + ((currency == null) ? 0 : currency.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -53,11 +44,17 @@ public class Amount {
 		if (amount != other.amount)
 			return false;
 		if (currency == null) {
-			if (other.currency != null)
-				return false;
-		} else if (!currency.equals(other.currency))
-			return false;
-		return true;
+			return other.currency == null;
+		} else return currency.equals(other.currency);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + amount;
+		result = prime * result + ((currency == null) ? 0 : currency.hashCode());
+		return result;
 	}
 
 }
