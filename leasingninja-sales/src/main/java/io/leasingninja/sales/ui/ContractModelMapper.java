602c4fe3816303ae8b1a99878fde93848848b5c3
Henning Schwentner <hs@wps.de>
2020-09-25 11:30:40 +0200

    Added precondition

--- leasingninja-sales/src/main/java/io/leasingninja/sales/ui/ContractModelMapper.java
+++ leasingninja-sales/src/main/java/io/leasingninja/sales/ui/ContractModelMapper.java
@@ -2,9 +2,13 @@ package io.leasingninja.sales.ui;
 
 import io.leasingninja.sales.domain.Contract;
 
+import java.util.Objects;
+
 class ContractModelMapper {
 
 	public static ContractModel modelFrom(Contract contract) {
+		Objects.requireNonNull(contract);
+		
 		return new ContractModel(
 				contract.number().value(),
 				contract.lessee().value(),
