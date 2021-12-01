d3cd4f5ad1eb383aaadfe8c5efc238fb0cda97a6
Henning Schwentner <hs@wps.de>
2021-12-01 21:25:20 +0100

    Added HP12c simulator

--- leasingninja-sales/src/main/java/io/leasingninja/sales/domain/FinancialCalculator.java
+++ leasingninja-sales/src/main/java/io/leasingninja/sales/domain/FinancialCalculator.java
@@ -8,8 +8,10 @@ import org.jmolecules.ddd.annotation.Service;
 @Service
 public class FinancialCalculator {
 
-	public static double pmt(double n, double i, double pv, double fv, double s) {
-		if (i == 0) {
+	public static double pmt(double n, double iInPercent, double pv, double fv, double s) {
+		double i = iInPercent / 100;
+        
+        if (i == 0) {
             return (-1 * pv - fv) / n;
         }
 
