b1cf94f0c319513a60805cedb9c5f5bfedb9fa3e
Henning Schwentner <hs@wps.de>
2023-01-05 14:36:40 +0100

    Cleanup spaces

--- leasingninja-sales/src/main/java/io/leasingninja/sales/domain/FinancialCalculator.java
+++ leasingninja-sales/src/main/java/io/leasingninja/sales/domain/FinancialCalculator.java
@@ -10,29 +10,29 @@ public class FinancialCalculator {
 
 	public static double pmt(double n, double iInPercent, double pv, double fv, double s) {
 		double i = iInPercent / 100;
-        
+
         if (i == 0) {
             return (-1 * pv - fv) / n;
         }
 
-		return (i * ( fv + pv * Math.pow(1 + i, n))) / ((1 + i * s) * (1 - Math.pow(1 + i, n)));
+		return (i * (fv + pv * Math.pow(1 + i, n))) / ((1 + i * s) * (1 - Math.pow(1 + i, n)));
 	}
-    
+
 }
 
 // Alternative implementation:
-		
-        /*		
+
+        /*
 		return
-			(-1 * pv * Math.pow(1 + i, n) - fv) 
+			(-1 * pv * Math.pow(1 + i, n) - fv)
 			/
 			(
-				(1 + i * s) 
+				(1 + i * s)
 				*
 				(
-					(Math.pow(1 + i, n) - 1) 
+					(Math.pow(1 + i, n) - 1)
 					/
 					i
 				)
 			);
-		*/		
+		*/
