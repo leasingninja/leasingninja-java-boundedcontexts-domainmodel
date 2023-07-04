301784a370bd9ac1064b54301fa197a4055f2e55
Henning Schwentner <hs@wps.de>
2023-07-04 16:06:03 +0200

    Add test case

--- leasingninja-sales/src/test/java/io/leasingninja/sales/domain/FinancialCalculatorTest.java
+++ leasingninja-sales/src/test/java/io/leasingninja/sales/domain/FinancialCalculatorTest.java
@@ -1,5 +1,19 @@
 package io.leasingninja.sales.domain;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class FinancialCalculatorTest {
+	@Test
+	void pmt() {
+		// given
+
+		// when
+		double pmt = FinancialCalculator.pmt(48, 3.7 / 12, -40_000, 0, 0);
+
+		// then
+		assertThat(pmt).isEqualTo(897.8022814470006);
+	}
 
 }
