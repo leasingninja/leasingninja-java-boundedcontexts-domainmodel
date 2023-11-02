8fd29a8d01f7014c0533e8ff1732a40305f2616b
Henning Schwentner <hs@wps.de>
2023-11-02 17:33:49 +0100

    Organize imports

--- leasingninja-riskmanagement/src/test/java/io/leasingninja/riskmanagement/application/InboxApplicationServiceTest.java
+++ leasingninja-riskmanagement/src/test/java/io/leasingninja/riskmanagement/application/InboxApplicationServiceTest.java
@@ -4,15 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.refEq;
 import static org.mockito.BDDMockito.then;
 
-import io.leasingninja.riskmanagement.domain.Contract;
-import io.leasingninja.riskmanagement.domain.ContractNumber;
-import io.leasingninja.riskmanagement.domain.ContractRepository;
-import io.leasingninja.riskmanagement.domain.SignDate;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import io.leasingninja.riskmanagement.domain.Contract;
+import io.leasingninja.riskmanagement.domain.ContractNumber;
+import io.leasingninja.riskmanagement.domain.ContractRepository;
+import io.leasingninja.riskmanagement.domain.SignDate;
 
 @ExtendWith(MockitoExtension.class)
 class InboxApplicationServiceTest {
