186146dcd236ca3c3ba60a690019d7d7c077dffb
Henning Schwentner <hs@wps.de>
2023-11-02 15:44:16 +0100

    Tweaking the financial calculator

--- leasingninja-sales/src/main/java/io/leasingninja/sales/domain/FinancialCalculator.java
+++ leasingninja-sales/src/main/java/io/leasingninja/sales/domain/FinancialCalculator.java
@@ -8,15 +8,35 @@ import org.jmolecules.ddd.annotation.Service;
 @Service
 public class FinancialCalculator {
 
+    /**
+     * @param n number of periods
+     * @param iInPercent percentage interest rate per period
+     * @param pv present value
+     * @param fv future value
+     * @param s
+     * @return payment per period
+     */
 	public static double pmt(double n, double iInPercent, double pv, double fv, double s) {
-		double i = iInPercent / 100;
+		double i = iInPercent / 100.0;
 
-        if (i == 0) {
-            return (-1 * pv - fv) / n;
+        return pmtWithDecimalInterestRate(n, i, pv, fv, s);
+	}
+
+    /**
+     * @param n number of periods
+     * @param i decimal interest rate per period
+     * @param pv present value
+     * @param fv future value
+     * @param s
+     * @return payment per period
+     */
+    private static double pmtWithDecimalInterestRate(double n, double i, double pv, double fv, double s) {
+        if (i == 0.0) {
+            return (-1.0 * pv - fv) / n;
         }
 
-		return (i * (fv + pv * Math.pow(1 + i, n))) / ((1 + i * s) * (1 - Math.pow(1 + i, n)));
-	}
+		return (i * (fv + pv * Math.pow(1.0 + i, n))) / ((1.0 + i * s) * (1.0 - Math.pow(1.0 + i, n)));
+    }
 
 }
 
