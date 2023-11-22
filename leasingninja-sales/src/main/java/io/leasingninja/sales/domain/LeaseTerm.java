5624bc5a3efe2fa96d0ab5e0b46ee4d1b6d13579
Henning Schwentner <hs@wps.de>
2023-11-22 17:05:51 +0100

    LeaseTerms have to be at least 1 month long

--- leasingninja-sales/src/main/java/io/leasingninja/sales/domain/LeaseTerm.java
+++ leasingninja-sales/src/main/java/io/leasingninja/sales/domain/LeaseTerm.java
@@ -4,8 +4,11 @@ import org.jmolecules.ddd.annotation.ValueObject;
 
 @ValueObject
 public record LeaseTerm(int noOfMonths) {
-    public static LeaseTerm ofMonths(int noOfMonths) {
+    public LeaseTerm {
         assert noOfMonths > 0;
+    }
+
+    public static LeaseTerm ofMonths(int noOfMonths) {
         return new LeaseTerm(noOfMonths);
     }
 
